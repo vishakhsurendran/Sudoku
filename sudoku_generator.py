a50ec15219022fce2f6158edc2b18a758a7e8dc1
APSA912 <149219392+APSA912@users.noreply.github.com>
2023-11-18 13:56:58 -0500

    Update sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -244,7 +244,6 @@ Return: list[list] (a 2D Python list to represent the board)
 def generate_sudoku(size, removed):
     sudoku = SudokuGenerator(size, removed)
     sudoku.fill_values()
-    board = sudoku.get_board()
     sudoku.remove_cells()
     board = sudoku.get_board()
     return board
