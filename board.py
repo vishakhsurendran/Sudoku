e3b45967ca16b65ef576c8532d51deb84473f6ac
APSA912 <149219392+APSA912@users.noreply.github.com>
2023-11-23 18:10:50 -0500

    add __init__ function to Board class

--- board.py
+++ board.py
@@ -5,4 +5,18 @@ from cell import Cell
 
 
 class Board:
+    # This is Samuel. I'll add my comments later
+    def __init__(self, width, height, screen, difficulty): # Samuel A.
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.difficulty = difficulty
+        self.cells = []
+        for row in range(9):
+            arr = []
+            for col in range(9):
+                cell = Cell((0, row, col, screen))
+                arr.append(cell)
+            self.cells.append(arr)
+        self.selected = None
     pass
